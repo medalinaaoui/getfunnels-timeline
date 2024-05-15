92e9b8f100b6b41d20c920dbbbad12c241bd7bee
Med Ali Naaoui <chayga00@gmail.com>
2024-05-16 00:04:31 +0100

    changing env

--- lib/axios.ts
+++ lib/axios.ts
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}`,
   headers: {
     "Content-Type": "application/json",
-    Authorization: "pk_81692187_S6V7840TIPCTHSR2J1UY1LZFTRGZCZYA",
+    Authorization: process.env.CLICKUP_API_KEY,
   },
 });
