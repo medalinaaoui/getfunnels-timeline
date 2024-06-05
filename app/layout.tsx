10d862b131ca7ddb5ab7b753e219efdabc6df41a
Med Ali Naaoui <chayga00@gmail.com>
2024-06-05 15:15:02 +0100

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,46 @@ import "@/app/ui/global.css";
 import TanStackProvider from "@/providers/TanStackProvider";
 import { PoppinsFont, Raitor } from "@/app/ui/fonts";
 import { Toaster } from "react-hot-toast";
+import type { Metadata } from "next";
+
+const title = "GetFunnels - Timeline";
+const description = "getfunnels timeline";
+const keywords = ["getfunnels", "digitalspeak"];
+export const metadata: Metadata = {
+  metadataBase: new URL(""),
+  // icons: ["./getfunnels-icon.png", "./getfunnels-icon.png"],
+  title,
+  description,
+  authors: [
+    {
+      name: "Mohamed Ali Naaoui",
+      url: "https://twitter.com/Med__Ali_",
+    },
+  ],
+  keywords,
+
+  openGraph: {
+    title,
+    description,
+    images: [
+      {
+        url: "./getfunnels-icon.png",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@these7enthart",
+    title,
+    description,
+    images: [
+      {
+        url: "./getfunnels-icon.png",
+      },
+    ],
+  },
+  robots: "index, follow",
+};
 
 export default function RootLayout({
   children,
