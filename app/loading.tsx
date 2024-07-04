cd2aaefc6ad1c4ced0f7cdf7ee73bdb6afe154dd
Med Ali Naaoui <chayga00@gmail.com>
2024-07-04 12:52:10 +0100

    loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,8 @@
-import Loader from "./components/Loader";
-
 const Loading = () => {
-  return <h1 className="bg-black text-white">main loader...</h1>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center z-[60] bg-primary">
+      <span className="loader-11"></span>
+    </div>
+  );
 };
 export default Loading;
